ad88c694c400005e7f916de0dceb0798cd0906b6
Stefan Larsson @mac <slarsson.gm@gmail.com>
2014-07-26 20:39:07 +0200

    Removed minification for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,18 @@ Rails.application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  class NoCompression
+    def compress(string)
+      # do nothing
+      string
+    end
+  end
+
+  config.assets.compress = true
+  config.assets.js_compressor = NoCompression.new
+  config.assets.css_compressor = NoCompression.new
+
+  #config.assets.js_compressor = :uglifier # !!! ORIGINAL !!!
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
