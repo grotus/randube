b5a32a13ba4f5cd3008ece7ed85b2e83afb3642d
Stefan Larsson @mac <slarsson.gm@gmail.com>
2014-07-26 20:13:12 +0200

    Heroku unfuck attempt 1

--- app/controllers/API/channels_controller.rb
+++ app/controllers/API/channels_controller.rb
@@ -1,7 +1,10 @@
-class Api::ChannelsController < ApplicationController
-	before_filter :authenticate_user!
+module Api
+	class ChannelsController < ApplicationController
+		before_filter :authenticate_user!
 
-	def index
-		render json: {channels: Channel.all}
+		def index
+			render json: {channels: Channel.all}
+		end
 	end
 end
+
